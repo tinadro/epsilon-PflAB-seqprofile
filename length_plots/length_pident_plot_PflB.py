5bcb37ecf219c251509550a101eb720022dffe4d
Ms Tina Drobnic <td1515@ls-msc01.bc.ic.ac.uk>
2019-06-20 16:17:12 +0100

    protein length plots

---
+++ length_plots/length_pident_plot_PflB.py
@@ -0,0 +1,55 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from matplotlib.ticker import MultipleLocator, AutoMinorLocator
+
+# make a dotplot x-slen, y-qcovs
+f = 'data-PflB.xlsx'
+df_bbh = pd.read_excel(f, sheet_name='BBH')
+df_bbh = df_bbh.loc[:, ['slen', 'pident']]
+x_bbh = df_bbh['slen'].apply(lambda x: x/820)
+y_bbh = df_bbh['pident']
+
+df_psi = pd.read_excel(f, sheet_name='psi')
+df_psi = df_psi.loc[:, ['slen', 'pident']]
+x_psi = df_psi['slen'].apply(lambda x: x/820)
+y_psi = df_psi['pident']
+
+#~~~~~~~~~~~~~~~~~~~~~~~
+# MAKE THE SCATTER PLOT:
+#~~~~~~~~~~~~~~~~~~~~~~~
+
+#make the plot
+fig, ax = plt.subplots()
+
+ax.scatter(x_bbh, y_bbh, color='#005bd3', marker='o', edgecolor='k', linewidth=0.7, label='bidirectional blastp hits')
+ax.scatter(x_psi, y_psi, color='C2', marker='^', edgecolor='k', linewidth=0.7, label='psiblast hits')
+
+# axis labels
+plt.xlabel('hit length/ query length ratio')
+plt.ylabel('% identity')
+
+#set xticks
+plt.xticks(np.arange(0.5, 1, 0.1))
+#ax.set_xlim(0.47, 1.13)
+#plt.yticks(range(60, 110, 10))
+#ax.set_ylim(58)
+
+#ax.xaxis.set_minor_locator(AutoMinorLocator(n=2))
+ax.yaxis.set_minor_locator(AutoMinorLocator(n=2)) # turn on minor ticks to be for every 1 bin
+
+#turn on y-axis gridlines
+ax.yaxis.grid(b=True, which='major', linestyle='--', color='0.9')
+ax.yaxis.grid(b=True, which='minor', linestyle='--', color='0.9')
+ax.set_axisbelow(True)
+
+
+#figure size
+fig.set_size_inches(6, 4)
+
+#position legend, show plot
+#plt.margins(1)
+plt.legend()
+plt.tight_layout()
+plt.savefig('PflB_slen_pident_scatter', dpi=300)
+plt.show()
--- length_plots/sequence_length_plot_PflA.py
+++
@@ -1,7 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-from os.path import dirname, abspath
-from matplotlib.ticker import MultipleLocator, AutoMinorLocator
-
-
